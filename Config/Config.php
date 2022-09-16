b3e63a71cdbe0889d8fbaffd76f7207ebf8b9d74
anerthy <112020687+anerthy@users.noreply.github.com>
2022-09-15 20:43:58 -0600

    config actualizado y borrado de imagenes basura

--- Config/Config.php
+++ Config/Config.php
@@ -1,21 +1,21 @@
-<?php 
-	
-	//define("BASE_URL", "http://localhost/paraiso_azul/");
-	const BASE_URL = "http://localhost/paraiso_azul";
+<?php
 
-	//Zona horaria
-	date_default_timezone_set('America/Costa_Rica');
+//define("BASE_URL", "http://localhost/paraiso_azul/");
+const BASE_URL = "http://localhost/paraiso_azul";
 
-	//Datos de conexión a Base de Datos
-	const DB_HOST = "localhost";
-	const DB_NAME = "db_paraisoazul";
-	const DB_USER = "root";
-	const DB_PASSWORD = "";
-	const DB_CHARSET = "utf8";
+//Zona horaria
+date_default_timezone_set('America/Costa_Rica');
 
-	//Deliminadores decimal y millar Ej. 24,1989.00
-	const SPD = ".";
-	const SPM = ",";
+//Datos de conexión a Base de Datos
+const DB_HOST = "localhost";
+const DB_NAME = "db_paraiso_azul";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+const DB_CHARSET = "utf8";
 
-	//Simbolo de moneda
-	const SMONEY = "₡";
+//Deliminadores decimal y millar Ej. 24,1989.00
+const SPD = ".";
+const SPM = ",";
+
+//Simbolo de moneda
+const SMONEY = "₡";
--- Views/home.php
+++ Views/home.php
@@ -1,10 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
- 
+
 </head>
+
 <body>
 
+  <div class="text-center">
+    <h1 class="display-4">Paraiso Azul - Golfo de Nicoya</h1>
+    <!-- @* <p>Learn about <a href="https://docs.microsoft.com/aspnet/core">building Web apps with ASP.NET Core</a>.</p>*@ -->
+  </div>
+
+  <div container>
+    <!-- <img src="<?= media(); ?>/images/img_13cd945b09b76c489d81b0fcdd9ff5c5.jpg" width="300" height="300" />
+    <br />
+    <br />
+    <br /> -->
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero libero, molestie et purus dapibus, elementum fermentum nunc. Curabitur sit amet nisl elit. Nulla luctus purus vel orci porttitor, condimentum lacinia sapien fermentum. Quisque diam libero, vulputate et tortor a, imperdiet rutrum libero. In nec nulla efficitur, auctor elit ut, molestie libero. Ut venenatis turpis tincidunt, dapibus justo et, finibus metus. Phasellus venenatis enim diam, ut cursus tellus tristique non. Fusce at mi laoreet, pharetra felis ut, iaculis metus. Donec suscipit elementum nibh non bibendum. Praesent sit amet felis eget diam posuere eleifend. Vestibulum mattis dictum tempor.
+
+      Cras sed leo lectus. Pellentesque nibh orci, tristique quis gravida non, dictum eget mi. Nulla in dui quam. Duis dui dolor, venenatis a pharetra non, dapibus a mauris. Quisque non bibendum ante. Vivamus ultrices consectetur purus, nec dapibus elit sodales non. Etiam ut odio rutrum, tincidunt dolor ac, ornare felis. Vivamus laoreet quam neque, at sollicitudin nisl lacinia vitae. Cras aliquet, nibh quis porttitor cursus, purus quam pellentesque lacus, vel euismod lorem nulla a tortor. Cras nec dui dolor.
+
+      Ut ac elit ex. Sed non nisi sollicitudin, scelerisque dolor sed, dapibus felis. Sed efficitur sollicitudin justo non ultrices. Integer ornare feugiat aliquet. Donec mattis faucibus mauris, mollis mollis erat varius quis. Vestibulum sollicitudin commodo ultricies. Vestibulum consequat ultrices libero eu pellentesque. Donec vitae vestibulum neque. Suspendisse sagittis posuere nisl eleifend cursus. Duis iaculis dui sem, vitae hendrerit quam aliquam eget. Curabitur ac felis volutpat, tempor lorem vel, faucibus ante.
+
+      Fusce posuere velit et convallis mattis. Suspendisse luctus imperdiet tellus, ut vestibulum odio bibendum nec. Donec vitae ipsum sed nibh scelerisque ultricies. Morbi vitae faucibus nunc. Integer sagittis nunc nec massa pretium pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi ac rhoncus quam. Suspendisse tempor faucibus neque at tempor. Fusce sit amet hendrerit orci. Suspendisse quis libero at urna sagittis tincidunt.
+
+      Donec auctor enim sit amet nisl rutrum, id vulputate quam volutpat. Aenean risus metus, tempus a mollis ultricies, rhoncus vel nulla. Nam dictum sed odio in condimentum. Morbi eu tempor massa. Maecenas gravida urna nec mauris luctus fermentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent dapibus magna semper, blandit tortor at, commodo nisl.
+    </p>
+  </div>
+
 </body>
+
 </html>
