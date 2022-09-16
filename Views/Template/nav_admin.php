1f00d4e799c1815406cef40355c5fc11e65349c8
anerthy <112020687+anerthy@users.noreply.github.com>
2022-09-15 20:52:45 -0600

    elimanacion de archivos de productos

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -1,56 +1,43 @@
     <!-- Sidebar menu-->
     <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
     <aside class="app-sidebar">
-      <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="<?= media();?>/images/avatar.png" alt="User Image">
-        <div>
-          <p class="app-sidebar__user-name">Usuario</p>
-          <p class="app-sidebar__user-designation">Administrador</p>
+        <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="<?= media(); ?>/images/avatar.png" alt="User Image">
+            <div>
+                <p class="app-sidebar__user-name">Usuario</p>
+                <p class="app-sidebar__user-designation">Administrador</p>
+            </div>
         </div>
-      </div>
-      <ul class="app-menu">
-        <li>
-            <a class="app-menu__item" href="<?= base_url(); ?>/dashboard">
-                <i class="app-menu__icon fa fa-dashboard"></i>
-                <span class="app-menu__label">Dashboard</span>
-            </a>
-        </li>
-        <li class="treeview">
-            <a class="app-menu__item" href="#" data-toggle="treeview">
-                <i class="app-menu__icon fa fa-users" aria-hidden="true"></i>
-                <span class="app-menu__label">Usuarios</span>
-                <i class="treeview-indicator fa fa-angle-right"></i>
-            </a>
-            <ul class="treeview-menu">
-                <li><a class="treeview-item" href="<?= base_url(); ?>/usuarios"><i class="icon fa fa-circle-o"></i> Usuarios</a></li>
-                <li><a class="treeview-item" href="<?= base_url(); ?>/roles"><i class="icon fa fa-circle-o"></i> Roles</a></li>
-                <li><a class="treeview-item" href="<?= base_url(); ?>/permisos"><i class="icon fa fa-circle-o"></i> Permisos</a></li>
-            </ul>
-        </li>
-        <!-- <li>
-            <a class="app-menu__item" href="<?= base_url(); ?>/clientes">
-                <i class="app-menu__icon fa fa-user" aria-hidden="true"></i>
-                <span class="app-menu__label">Clientes</span>
-            </a>
-        </li> -->
-        <li>
-            <a class="app-menu__item" href="<?= base_url(); ?>/products">
-        
-                <i class="app-menu__icon fa fa-fish" aria-hidden="true"></i>
-                <span class="app-menu__label">Productos</span>
-            </a>
-        </li>
-          <li>
-            <a class="app-menu__item" href="<?= base_url(); ?>/grupos">
-          
-                <i class="app-menu__icon fa  fa-city" aria-hidden="true"></i>
-                <span class="app-menu__label">Grupos Organizados</span>
-            </a>
-        </li>
-        <li>
-            <a class="app-menu__item" href="<?= base_url(); ?>/logout">
-                <i class="app-menu__icon fa fa-sign-out" aria-hidden="true"></i>
-                <span class="app-menu__label">Logout</span>
-            </a>
-        </li>
-      </ul>
+        <ul class="app-menu">
+            <li>
+                <a class="app-menu__item" href="<?= base_url(); ?>/dashboard">
+                    <i class="app-menu__icon fa fa-dashboard"></i>
+                    <span class="app-menu__label">Dashboard</span>
+                </a>
+            </li>
+            <li class="treeview">
+                <a class="app-menu__item" href="#" data-toggle="treeview">
+                    <i class="app-menu__icon fa fa-users" aria-hidden="true"></i>
+                    <span class="app-menu__label">Usuarios</span>
+                    <i class="treeview-indicator fa fa-angle-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a class="treeview-item" href="<?= base_url(); ?>/usuarios"><i class="icon fa fa-circle-o"></i> Usuarios</a></li>
+                    <li><a class="treeview-item" href="<?= base_url(); ?>/roles"><i class="icon fa fa-circle-o"></i> Roles</a></li>
+                    <li><a class="treeview-item" href="<?= base_url(); ?>/permisos"><i class="icon fa fa-circle-o"></i> Permisos</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="app-menu__item" href="<?= base_url(); ?>/grupos">
+
+                    <i class="app-menu__icon fa  fa-city" aria-hidden="true"></i>
+                    <span class="app-menu__label">Grupos Organizados</span>
+                </a>
+            </li>
+            <li>
+                <a class="app-menu__item" href="<?= base_url(); ?>/logout">
+                    <i class="app-menu__icon fa fa-sign-out" aria-hidden="true"></i>
+                    <span class="app-menu__label">Logout</span>
+                </a>
+            </li>
+        </ul>
     </aside>
