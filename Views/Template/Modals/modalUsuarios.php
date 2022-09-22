dc9d9771fdc46d6c91596d0b91c56834fa4caec5
anerthy <112020687+anerthy@users.noreply.github.com>
2022-09-22 16:01:21 -0600

    vistas de usuario

--- Views/Template/Modals/modalUsuarios.php
+++ Views/Template/Modals/modalUsuarios.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="modalFormUsuario" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg" >
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header headerRegister">
         <h5 class="modal-title" id="titleModal">Nuevo Usuario</h5>
@@ -9,61 +9,52 @@
         </button>
       </div>
       <div class="modal-body">
-            <form id="formUsuario" name="formUsuario" class="form-horizontal">
-              <input type="hidden" id="idUsuario" name="idUsuario" value="">
-              <p class="text-primary">Todos los campos son obligatorios.</p>
+        <form id="formUsuario" name="formUsuario" class="form-horizontal">
+          <input type="hidden" id="idUsuario" name="idUsuario" value="">
+          <p class="text-primary">Todos los campos son obligatorios.</p>
 
-              <div class="form-row">
+          <!-- <div class="form-row">
                 <div class="form-group col-md-6">
                   <label for="txtIdentificacion">Identificación</label>
                   <input type="text" class="form-control" id="txtIdentificacion" name="txtIdentificacion" required="">
                 </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="txtNombre">Nombres</label>
-                  <input type="text" class="form-control valid validText" id="txtNombre" name="txtNombre" required="">
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="txtApellido">Apellidos</label>
-                  <input type="text" class="form-control valid validText" id="txtApellido" name="txtApellido" required="">
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="txtTelefono">Teléfono</label>
-                  <input type="text" class="form-control valid validNumber" id="txtTelefono" name="txtTelefono" required="" onkeypress="return controlTag(event);">
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="txtEmail">Email</label>
-                  <input type="email" class="form-control valid validEmail" id="txtEmail" name="txtEmail" required="">
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="listRolid">Tipo usuario</label>
-                    <select class="form-control" data-live-search="true" id="listRolid" name="listRolid" required >
-                    </select>
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="listStatus">Status</label>
-                    <select class="form-control selectpicker" id="listStatus" name="listStatus" required >
-                        <option value="1">Activo</option>
-                        <option value="2">Inactivo</option>
-                    </select>
-                </div>
-             </div>
-             <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="txtPassword">Password</label>
-                  <input type="password" class="form-control" id="txtPassword" name="txtPassword" >
-                </div>
-             </div>
-              <div class="tile-footer">
-                <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;
-                <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cerrar</button>
-              </div>
-            </form>
+              </div> -->
+
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="txtNombre">Nombre</label>
+              <input type="text" class="form-control valid validText" id="txtNombre" name="txtNombre" required="">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="txtCorreo">Correo</label>
+              <input type="email" class="form-control valid validEmail" id="txtCorreo" name="txtCorreo" required="">
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="listRolid">Rol del usuario</label>
+              <select class="form-control" data-live-search="true" id="listRolid" name="listRolid" required>
+              </select>
+            </div>
+            <div class="form-group col-md-6">
+              <label for="listStatus">Estado</label>
+              <select class="form-control selectpicker" id="listStatus" name="listStatus" required>
+                <option value="1">Activo</option>
+                <option value="2">Inactivo</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="txtContraseña">Contraseña</label>
+              <input type="password" class="form-control" id="txtContraseña" name="txtContraseña">
+            </div>
+          </div>
+          <div class="tile-footer">
+            <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;
+            <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cerrar</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -71,7 +62,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modalViewUser" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog" >
+  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header header-primary">
         <h5 class="modal-title" id="titleModal">Datos del usuario</h5>
@@ -83,36 +74,21 @@
         <table class="table table-bordered">
           <tbody>
             <tr>
-              <td>Identificación:</td>
-              <td id="celIdentificacion">654654654</td>
-            </tr>
-            <tr>
               <td>Nombres:</td>
               <td id="celNombre">Jacob</td>
             </tr>
+
             <tr>
-              <td>Apellidos:</td>
-              <td id="celApellido">Jacob</td>
+              <td>Correo (Usuario):</td>
+              <td id="celCorreo">Larry</td>
             </tr>
             <tr>
-              <td>Teléfono:</td>
-              <td id="celTelefono">Larry</td>
-            </tr>
-            <tr>
-              <td>Email (Usuario):</td>
-              <td id="celEmail">Larry</td>
-            </tr>
-            <tr>
-              <td>Tipo Usuario:</td>
+              <td>Rol de Usuario:</td>
               <td id="celTipoUsuario">Larry</td>
             </tr>
             <tr>
               <td>Estado:</td>
               <td id="celEstado">Larry</td>
-            </tr>
-            <tr>
-              <td>Fecha registro:</td>
-              <td id="celFechaRegistro">Larry</td>
             </tr>
           </tbody>
         </table>
@@ -123,4 +99,3 @@
     </div>
   </div>
 </div>
-
