a5d50649d53ab5b4123ca70495ac27c4676b1e26
anerthy <112020687+anerthy@users.noreply.github.com>
2022-09-25 16:57:29 -0600

    nav bar sin usuario

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -14,18 +14,7 @@
                     <span class="app-menu__label">Dashboard</span>
                 </a>
             </li>
-            <li class="treeview">
-                <a class="app-menu__item" href="#" data-toggle="treeview">
-                    <i class="app-menu__icon fa fa-users" aria-hidden="true"></i>
-                    <span class="app-menu__label">Usuarios</span>
-                    <i class="treeview-indicator fa fa-angle-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a class="treeview-item" href="<?= base_url(); ?>/usuarios"><i class="icon fa fa-circle-o"></i> Usuarios</a></li>
-                    <li><a class="treeview-item" href="<?= base_url(); ?>/roles"><i class="icon fa fa-circle-o"></i> Roles</a></li>
-                    <!-- <li><a class="treeview-item" href="<?= base_url(); ?>/permisos"><i class="icon fa fa-circle-o"></i> Permisos</a></li> -->
-                </ul>
-            </li>
+
             <li>
                 <a class="app-menu__item" href="<?= base_url(); ?>/grupos">
 
