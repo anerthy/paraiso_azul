45c7c2fcd52f7bef5e659b3bf29a20968df2e9d8
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2022-10-05 13:26:12 -0600

    actualizado

---
+++ Views/Tbl_paginas/tbl_paginas.php
@@ -0,0 +1,51 @@
+<?php
+headerAdmin($data);
+getModal('modalTbl_paginas', $data);
+?>
+<div id="contentAjax"></div>
+<main class="app-content">
+  <div class="app-title">
+    <div>
+      <h1><i class="fas fa-city"></i> <?= $data['page_title'] ?>
+        <button class="btn btn-primary" type="button" onclick="openModal();"><i class="fas fa-plus-circle"></i> Nuevo</button>
+      </h1>
+    </div>
+    <ul class="app-breadcrumb breadcrumb">
+      <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+      <li class="breadcrumb-item"><a href="<?= base_url(); ?>/tbl_paginas"><?= $data['page_title'] ?></a></li>
+    </ul>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="tile">
+        <div class="tile-body">
+          <div class="table-responsive">
+            <table class="table table-hover table-bordered" id="tableTbl_paginas">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>Titulo</th>
+                  <th>Contenido</th>
+                  <!-- <th></th> -->
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Patitos</td>
+                  <td>PatitosDescripcion</td>
+                  <td>Al lado del bnc</td>
+                  <!-- <td></td> -->
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<?php footerAdmin($data); ?>
