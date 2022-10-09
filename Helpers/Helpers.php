110fe408f825da4624affa037e27bb0cade2f00b
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-08 22:41:29 -0600

    agrega obtener permisos

--- Helpers/Helpers.php
+++ Helpers/Helpers.php
@@ -75,7 +75,7 @@ function getPermisos(int $idmodulo)
 {
     require_once("Models/PermisosModel.php");
     $objPermisos = new PermisosModel();
-    $idrol = $_SESSION['userData']['idrol'];
+    $idrol = $_SESSION['userData']['id_rol'];
     $arrPermisos = $objPermisos->permisosModulo($idrol);
     $permisos = '';
     $permisosMod = '';
