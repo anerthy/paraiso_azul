87b962082c49f7d6afa9a2a016537fa1d22e7963
GlendaSofia <113810729+GlendaSofia@users.noreply.github.com>
2022-10-16 17:52:12 -0600

    TTransporte.php

---
+++ Views/SobreNosotros/transporte.php
@@ -0,0 +1,31 @@
+<?php
+_header($data);
+$arrTransporte =  $data['transporte'];
+?>
+<div>
+    <h1>¿Que son los transporte Organizados?</h1><br>
+    <p id="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officia eius qui, atque voluptatem, voluptatum quae impedit quasi autem repellendus pariatur labore quod eos totam modi. Eveniet nesciunt rem non? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate doloremque ea voluptates et eveniet est. Provident earum tempore ratione dolorum! Distinctio ex quas illo tenetur aspernatur reprehenderit earum voluptate voluptatum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem corrupti iure ipsa esse hic sed repellendus minus dolorum tempore, totam doloribus tempora, odit eius sit ad expedita dolores rerum nesciunt!</p>
+</div>
+
+<div class="form-row">
+    <?php
+    // dep($arrTransporte);
+    for ($i = 0; $i < count($arrTransporte); $i++) {
+        $arrTransporte[$i]['logo'];
+    ?>
+        <div id="cardtransporte" class="card col-md-3 justify-content-center">
+            <img id="logo" src="<?= $arrTransporte[$i]['logo'] ?>" alt="logo del transporte" class="card-img-top" alt="logo del transporte" style="width: 300px; height:300px;">
+            <div>
+                <h4 class="nombre"><?= $arrTransporte[$i]['nombre_transporte'] ?></h4>
+                <p id="descripcion" class="card-text"><?= $arrTransporte[$i]['descripcion'] ?></p>
+                <center><a href="#" class="btn btn-info">Ver más informacion</a></center>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
+</div>
+
+<?php
+footer($data);
+?>
