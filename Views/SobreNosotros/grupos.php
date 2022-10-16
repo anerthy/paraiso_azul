0fb6fbaf11396088cedfa9b2969cfb63878f321d
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-15 22:52:46 -0600

    vista de grupos con cards

--- Views/SobreNosotros/grupos.php
+++ Views/SobreNosotros/grupos.php
@@ -2,39 +2,29 @@
 _header($data);
 $arrGrupos =  $data['cardgrupos'];
 ?>
+<div>
+    <h1>¿Que son los Grupos Organizados?</h1><br>
+    <p id="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officia eius qui, atque voluptatem, voluptatum quae impedit quasi autem repellendus pariatur labore quod eos totam modi. Eveniet nesciunt rem non? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate doloremque ea voluptates et eveniet est. Provident earum tempore ratione dolorum! Distinctio ex quas illo tenetur aspernatur reprehenderit earum voluptate voluptatum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem corrupti iure ipsa esse hic sed repellendus minus dolorum tempore, totam doloribus tempora, odit eius sit ad expedita dolores rerum nesciunt!</p>
+</div>
 
-<h1>¿Que son los Grupos Organizados?</h1><br>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officia eius qui, atque voluptatem, voluptatum quae impedit quasi autem repellendus pariatur labore quod eos totam modi. Eveniet nesciunt rem non? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate doloremque ea voluptates et eveniet est. Provident earum tempore ratione dolorum! Distinctio ex quas illo tenetur aspernatur reprehenderit earum voluptate voluptatum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem corrupti iure ipsa esse hic sed repellendus minus dolorum tempore, totam doloribus tempora, odit eius sit ad expedita dolores rerum nesciunt!</p>
-
-<?php
-// dep($arrGrupos);
-for ($i = 0; $i < count($arrGrupos); $i++) {
-    $arrGrupos[$i]['logo'];
-?>
-
-    <div style="margin: 10%;">
-        <img src="<?= $arrGrupos[$i]['logo'] ?>" alt="logo del grupo" width="100">
-        <h2><?= $arrGrupos[$i]['nombre_grupo'] ?></h2>
-        <h3>Representante: <?= $arrGrupos[$i]['representante'] ?></h3>
-        <h3>¿Que es <?= $arrGrupos[$i]['nombre_grupo'] ?>?</h3>
-        <p><?= $arrGrupos[$i]['descripcion'] ?></p>
-        <div>
-            <h3>Ubicacion</h3>
-            <h3><?= $arrGrupos[$i]['nombre_com'] ?></h3>
-            <p><?= $arrGrupos[$i]['ubicacion'] ?></p>
+<div class="form-row">
+    <?php
+    // dep($arrGrupos);
+    for ($i = 0; $i < count($arrGrupos); $i++) {
+        $arrGrupos[$i]['logo'];
+    ?>
+        <div id="cardgrupos" class="card col-md-3 justify-content-center">
+            <img id="logo" src="<?= $arrGrupos[$i]['logo'] ?>" alt="logo del grupo" class="card-img-top" alt="logo del grupo" style="width: 300px; height:300px;">
+            <div>
+                <h4 class="nombre"><?= $arrGrupos[$i]['nombre_grupo'] ?></h4>
+                <p id="descripcion" class="card-text"><?= $arrGrupos[$i]['descripcion'] ?></p>
+                <center><a href="#" class="btn btn-info">Ver más informacion</a></center>
+            </div>
         </div>
-
-        <div>
-            <h3>Contacto:</h3>
-            <h3><?= $arrGrupos[$i]['correo'] ?></h3>
-            <h3><?= $arrGrupos[$i]['telefono'] ?></h3>
-        </div>
-        <h3>Cantidad de integrantes: <?= $arrGrupos[$i]['numero_integrantes'] ?></h3>
-    </div>
-
-<?php
-}
-?>
+    <?php
+    }
+    ?>
+</div>
 
 <?php
 footer($data);
