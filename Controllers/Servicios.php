ef9528d8158efa47656ede111f19bd47cc910509
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2022-10-16 12:35:50 -0600

    Creacion de funcion hospedaje

--- Controllers/Servicios.php
+++ Controllers/Servicios.php
@@ -1,7 +1,8 @@
 <?php
-
+require_once("Models/Traits/THospedaje.php");
 class Servicios extends Controllers
 {
+    use THospedaje;
     public function __construct()
     {
         parent::__construct();
@@ -19,13 +20,13 @@ class Servicios extends Controllers
 
     public function hospedaje()
     {
-        $data['page_id'] = 2;
-        $data['page_tag'] = "hospedaje";
-        $data['page_title'] = "Servicio de Hospedaje";
-        $data['page_name'] = "hospedaje";
-        $data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quis. Perspiciatis repellat perferendis accusamus, ea natus id omnis, ratione alias quo dolore tempore dicta cum aliquid corrupti enim deserunt voluptas.";
+        $data['page_tag'] = "Hospedaje";
+        $data['page_title'] = "Hospedaje";
+        $data['page_name'] = "viewhospedajes";
+        $data['hospedaje'] = $this->getHospedajesT();
         $this->views->getView($this, "hospedaje", $data);
     }
+  
 
     public function transporte()
     {
