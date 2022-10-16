76f1aab3c214edea81317548703c5d689e8271c0
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-15 20:55:31 -0600

    vista actualizada

--- Views/SobreNosotros/grupos.php
+++ Views/SobreNosotros/grupos.php
@@ -5,25 +5,36 @@ $arrGrupos =  $data['cardgrupos'];
 
 <h1>¿Que son los Grupos Organizados?</h1><br>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officia eius qui, atque voluptatem, voluptatum quae impedit quasi autem repellendus pariatur labore quod eos totam modi. Eveniet nesciunt rem non? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate doloremque ea voluptates et eveniet est. Provident earum tempore ratione dolorum! Distinctio ex quas illo tenetur aspernatur reprehenderit earum voluptate voluptatum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem corrupti iure ipsa esse hic sed repellendus minus dolorum tempore, totam doloribus tempora, odit eius sit ad expedita dolores rerum nesciunt!</p>
-<br>
-<br>
-<div class="container">
-    <?php
-    // dep($arrGrupos);
-    for ($i = 0; $i < count($arrGrupos); $i++) {
-        $arrGrupos[$i]['logo'];
-    ?>
+
+<?php
+// dep($arrGrupos);
+for ($i = 0; $i < count($arrGrupos); $i++) {
+    $arrGrupos[$i]['logo'];
+?>
+
+    <div style="margin: 10%;">
+        <img src="<?= $arrGrupos[$i]['logo'] ?>" alt="logo del grupo" width="100">
+        <h2><?= $arrGrupos[$i]['nombre_grupo'] ?></h2>
+        <h3>Representante: <?= $arrGrupos[$i]['representante'] ?></h3>
+        <h3>¿Que es <?= $arrGrupos[$i]['nombre_grupo'] ?>?</h3>
+        <p><?= $arrGrupos[$i]['descripcion'] ?></p>
+        <div>
+            <h3>Ubicacion</h3>
+            <h3><?= $arrGrupos[$i]['nombre_com'] ?></h3>
+            <p><?= $arrGrupos[$i]['ubicacion'] ?></p>
+        </div>
 
         <div>
-            <h4 class="mb-0"><?= $arrGrupos[$i]['nombre_grupo'] ?></h4 <p class="mb-4"><?= $arrGrupos[$i]['descripcion'] ?></p>
-            <img class="img-fluid" src="<?= $arrGrupos[$i]['logo'] ?>" alt="logo del grupo" width="100">
+            <h3>Contacto:</h3>
+            <h3><?= $arrGrupos[$i]['correo'] ?></h3>
+            <h3><?= $arrGrupos[$i]['telefono'] ?></h3>
         </div>
-        <br>
+        <h3>Cantidad de integrantes: <?= $arrGrupos[$i]['numero_integrantes'] ?></h3>
+    </div>
 
-    <?php
-    }
-    ?>
-</div>
+<?php
+}
+?>
 
 <?php
 footer($data);
