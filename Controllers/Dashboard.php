5f779570a2e561d6f99c4e0abc7cec6f593c950b
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2022-10-16 18:50:24 -0600

    Cambie la funcion session_start por sessionStart

--- Controllers/Dashboard.php
+++ Controllers/Dashboard.php
@@ -4,10 +4,12 @@ class Dashboard extends Controllers
 {
 	public function __construct()
 	{
+		sessionStart();
 		parent::__construct();
 
-		session_start();
-		session_regenerate_id(true);
+		 
+		//session_start();
+		//session_regenerate_id(true);
 
 		if (empty($_SESSION['login'])) {
 			header('Location: ' . base_url() . '/login');
