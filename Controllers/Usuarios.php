238fcbd6e2c0fe1e5574a956ab40d7c45e6cb53a
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-16 21:06:06 -0600

    se restinge para que no se pueda acceder desde url

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -4,9 +4,8 @@ class Usuarios extends Controllers
 {
 	public function __construct()
 	{
-		sessionStart();
 		parent::__construct();
-		//session_start();
+		sessionStart();
 		//session_regenerate_id(true);
 		if (empty($_SESSION['login'])) {
 			header('Location: ' . base_url() . '/login');
@@ -28,8 +27,9 @@ class Usuarios extends Controllers
 
 	public function setUsuario()
 	{
-		if ($_SESSION['permisosMod']['agregar']) {
-			if ($_POST) {
+		if ($_POST) {
+			if (($_SESSION['permisosMod']['agregar']) || ($_SESSION['permisosMod']['actualizar'])) {
+
 				if (empty($_POST['txtNombre']) || empty($_POST['txtCorreo']) || empty($_POST['listRolid']) || empty($_POST['listStatus'])) {
 					$arrResponse = array("status" => false, "msg" => 'Datos incorrectos.');
 				} else {
@@ -82,63 +82,65 @@ class Usuarios extends Controllers
 
 	public function getUsuarios()
 	{
+		if ($_SESSION['permisosMod']['ver']) {
+			$arrData = $this->model->selectUsuarios();
+			for ($i = 0; $i < count($arrData); $i++) {
+				$btnView = '';
+				$btnEdit = '';
+				$btnDelete = '';
 
-		$arrData = $this->model->selectUsuarios();
-		for ($i = 0; $i < count($arrData); $i++) {
-			$btnView = '';
-			$btnEdit = '';
-			$btnDelete = '';
-
-			if ($arrData[$i]['status'] == 1) {
-				$arrData[$i]['status'] = '<span class="badge badge-success">Activo</span>';
-			} else {
-				$arrData[$i]['status'] = '<span class="badge badge-danger">Inactivo</span>';
-			}
-
-			// boton de ver
-			if ($_SESSION['permisosMod']['ver']) {
-				$btnView = '<button class="btn btn-info btn-sm btnViewUsuario" onClick="fntViewUsuario(' . $arrData[$i]['id_usuario'] . ')" title="Ver usuario"><i class="far fa-eye"></i></button>';
-			}
-			// boton de actualizar
-			if ($_SESSION['permisosMod']['actualizar']) {
-				if (($_SESSION['idUser'] == 1 and $_SESSION['userData']['id_rol'] == 1) ||
-					($_SESSION['userData']['id_rol'] == 1 and $arrData[$i]['id_rol'] != 1)
-				) {
-					$btnEdit = '<button class="btn btn-primary  btn-sm btnEditUsuario" onClick="fntEditUsuario(this,' . $arrData[$i]['id_usuario'] . ')" title="Editar usuario"><i class="fas fa-pencil-alt"></i></button>';
+				if ($arrData[$i]['status'] == 1) {
+					$arrData[$i]['status'] = '<span class="badge badge-success">Activo</span>';
 				} else {
-					$btnEdit = '<button class="btn btn-secondary btn-sm" disabled ><i class="fas fa-pencil-alt"></i></button>';
+					$arrData[$i]['status'] = '<span class="badge badge-danger">Inactivo</span>';
 				}
-			}
-			// boton de eliminar
-			if ($_SESSION['permisosMod']['eliminar']) {
-				if (($_SESSION['idUser'] == 1 and $_SESSION['userData']['id_rol'] == 1) ||
-					($_SESSION['userData']['id_rol'] == 1 and $arrData[$i]['id_rol'] != 1) and
-					($_SESSION['userData']['id_usuario'] != $arrData[$i]['id_usuario'])
-				) {
-					$btnDelete = '<button class="btn btn-danger btn-sm btnDelUsuario" onClick="fntDelUsuario(' . $arrData[$i]['id_usuario'] . ')" title="Eliminar usuario"><i class="far fa-trash-alt"></i></button>';
-				} else {
-					$btnDelete = '<button class="btn btn-secondary btn-sm" disabled ><i class="far fa-trash-alt"></i></button>';
-				}
-			}
 
-			$arrData[$i]['options'] = '<div class="text-center">' . $btnView . ' ' . $btnEdit . ' ' . $btnDelete . '</div>';
+				// boton de ver
+				if ($_SESSION['permisosMod']['ver']) {
+					$btnView = '<button class="btn btn-info btn-sm btnViewUsuario" onClick="fntViewUsuario(' . $arrData[$i]['id_usuario'] . ')" title="Ver usuario"><i class="far fa-eye"></i></button>';
+				}
+				// boton de actualizar
+				if ($_SESSION['permisosMod']['actualizar']) {
+					if (($_SESSION['idUser'] == 1 and $_SESSION['userData']['id_rol'] == 1) ||
+						($_SESSION['userData']['id_rol'] == 1 and $arrData[$i]['id_rol'] != 1)
+					) {
+						$btnEdit = '<button class="btn btn-primary  btn-sm btnEditUsuario" onClick="fntEditUsuario(this,' . $arrData[$i]['id_usuario'] . ')" title="Editar usuario"><i class="fas fa-pencil-alt"></i></button>';
+					} else {
+						$btnEdit = '<button class="btn btn-secondary btn-sm" disabled ><i class="fas fa-pencil-alt"></i></button>';
+					}
+				}
+				// boton de eliminar
+				if ($_SESSION['permisosMod']['eliminar']) {
+					if (($_SESSION['idUser'] == 1 and $_SESSION['userData']['id_rol'] == 1) ||
+						($_SESSION['userData']['id_rol'] == 1 and $arrData[$i]['id_rol'] != 1) and
+						($_SESSION['userData']['id_usuario'] != $arrData[$i]['id_usuario'])
+					) {
+						$btnDelete = '<button class="btn btn-danger btn-sm btnDelUsuario" onClick="fntDelUsuario(' . $arrData[$i]['id_usuario'] . ')" title="Eliminar usuario"><i class="far fa-trash-alt"></i></button>';
+					} else {
+						$btnDelete = '<button class="btn btn-secondary btn-sm" disabled ><i class="far fa-trash-alt"></i></button>';
+					}
+				}
+
+				$arrData[$i]['options'] = '<div class="text-center">' . $btnView . ' ' . $btnEdit . ' ' . $btnDelete . '</div>';
+			}
+			echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
 		}
-		echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
 		die();
 	}
 
-	public function getUsuario(int $idusuario)
+	public function getUsuario($idusuario)
 	{
-
-		$idusuario = intval($idusuario);
-		if ($idusuario > 0) {
-			$arrData = $this->model->selectUsuario($idusuario);
-			if (empty($arrData)) {
-				$arrResponse = array('status' => false, 'msg' => 'Datos no encontrados.');
-			} else {
-				$arrResponse = array('status' => true, 'data' => $arrData);
+		if ($_SESSION['permisosMod']['ver']) {
+			$idusuario = intval($idusuario);
+			if ($idusuario > 0) {
+				$arrData = $this->model->selectUsuario($idusuario);
+				if (empty($arrData)) {
+					$arrResponse = array('status' => false, 'msg' => 'Datos no encontrados.');
+				} else {
+					$arrResponse = array('status' => true, 'data' => $arrData);
+				}
+				echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
 			}
-			echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
 		}
 		die();
 	}
@@ -146,14 +148,16 @@ class Usuarios extends Controllers
 	public function delUsuario()
 	{
 		if ($_POST) {
-			$intIdusuario = intval($_POST['idUsuario']);
-			$requestDelete = $this->model->deleteUsuario($intIdusuario);
-			if ($requestDelete) {
-				$arrResponse = array('status' => true, 'msg' => 'Se ha eliminado el usuario');
-			} else {
-				$arrResponse = array('status' => false, 'msg' => 'Error al eliminar el usuario.');
+			if ($_SESSION['permisosMod']['eliminar']) {
+				$intIdusuario = intval($_POST['idUsuario']);
+				$requestDelete = $this->model->deleteUsuario($intIdusuario);
+				if ($requestDelete) {
+					$arrResponse = array('status' => true, 'msg' => 'Se ha eliminado el usuario');
+				} else {
+					$arrResponse = array('status' => false, 'msg' => 'Error al eliminar el usuario.');
+				}
+				echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
 			}
-			echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
 		}
 		die();
 	}
