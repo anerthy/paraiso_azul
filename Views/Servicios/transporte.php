bd9b9ab43cccabf9d9710a3bd05bcd9a10441058
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-16 18:13:34 -0600

    se llama el array en las vistas

--- Views/Servicios/transporte.php
+++ Views/Servicios/transporte.php
@@ -1,9 +1,11 @@
 <?php
 _header($data);
+$arrTransportes = $data['transportes'];
 ?>
 
 <h1>Transporte</h1>
 
 <?php
+// dep($arrTransportes);
 footer($data);
 ?>
--- Views/SobreNosotros/comunidades.php
+++ Views/SobreNosotros/comunidades.php
@@ -1,9 +1,11 @@
 <?php
 _header($data);
+$arrComunidades = $data['comunidades'];
 ?>
 
 <h1>Comunidades</h1>
 
 <?php
+// dep($arrComunidades);
 footer($data);
 ?>
