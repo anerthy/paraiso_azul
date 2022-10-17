2e1c4877ceed425db508ce0691046ec090282168
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-16 21:12:16 -0600

    se movio el session start

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -4,8 +4,8 @@ class Usuarios extends Controllers
 {
 	public function __construct()
 	{
-		parent::__construct();
 		sessionStart();
+		parent::__construct();
 		//session_regenerate_id(true);
 		if (empty($_SESSION['login'])) {
 			header('Location: ' . base_url() . '/login');
