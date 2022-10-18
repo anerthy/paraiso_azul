fb07439142b7e3d487d04f15e5ef8550f648c63d
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-17 18:07:26 -0600

    se agrega un enlace para acceder al dashboard

--- Views/Template/nav_bar.php
+++ Views/Template/nav_bar.php
@@ -3,14 +3,14 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
   <div class="container">
     <a class="navbar-brand" href="#">
-      
+
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto">
-      
+
 
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="<?= base_url(); ?>">Inicio</a>
@@ -18,28 +18,28 @@
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          ¿Que ofrecemos?
+            ¿Que ofrecemos?
           </a>
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="<?= base_url(); ?>/Servicios/alimentacion">Alimentacion</a></li>
             <li><a class="dropdown-item" href="<?= base_url(); ?>/Servicios/hospedaje">Hospedaje</a></li>
             <li><a class="dropdown-item" href="<?= base_url(); ?>/Servicios/transporte">Transporte</a></li>
             <li><a class="dropdown-item" href="<?= base_url(); ?>/Servicios/tours">Tours</a></li>
-            <li>         
-            </li>    
+            <li>
+            </li>
           </ul>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Sobre el proyecto
+            Sobre el proyecto
           </a>
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="<?= base_url(); ?>/SobreNosotros/cemede">CEMEDE</a></li>
             <li><a class="dropdown-item" href="<?= base_url(); ?>/SobreNosotros/grupos">Grupos Organizados</a></li>
             <li><a class="dropdown-item" href="<?= base_url(); ?>/SobreNosotros/comunidades">Comunidades</a></li>
-            <li>         
-            </li>    
+            <li>
+            </li>
           </ul>
         </li>
 
@@ -47,7 +47,10 @@
           <a class="nav-link" href="<?= base_url(); ?>/voluntariado">Voluntariado</a>
         </li>
 
- 
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url(); ?>/login">Login</a>
+        </li>
+
       </ul>
     </div>
   </div>
