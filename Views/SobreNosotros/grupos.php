a48f56488cfd0691ca4329614b0fd37ac8a01a3c
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-23 21:36:54 -0600

    actualiza vista grupos

--- Views/SobreNosotros/grupos.php
+++ Views/SobreNosotros/grupos.php
@@ -21,7 +21,7 @@ $arrGrupos =  $data['grupos'];
         for ($i = 0; $i < count($arrGrupos); $i++) {
             // $arrGrupos[$i]['logo'];
         ?>
-            <div id="cardgrupos" class="card col-md-3 justify-content-center">
+            <div id="cardgrupos" class="col-md-3 col-sm-3 col-xs-12 justify-content-center">
                 <img id="logo" src="<?= $arrGrupos[$i]['logo'] ?>" alt="logo del grupo" class="card-img-top" alt="logo del grupo" style="width: 300px; height:300px;">
                 <div>
                     <h4 class="nombre"><?= $arrGrupos[$i]['nombre_grupo'] ?></h4>
