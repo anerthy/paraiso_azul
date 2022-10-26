af871dd25b27dbf6f4929be95bfa77dfb668463f
anerthy <112020687+anerthy@users.noreply.github.com>
2022-10-26 00:04:43 -0600

    card de comunidades

--- Views/SobreNosotros/comunidades.php
+++ Views/SobreNosotros/comunidades.php
@@ -3,27 +3,71 @@ _header($data);
 $arrComunidades = $data['comunidades'];
 ?>
 
-<h1>Comunidades</h1>
+<main>
 
-<div class="row">
-    <?php
-    // dep($arrTransporte);
-    for ($i = 0; $i < count($arrComunidades); $i++) {
-        // $arrComunidades[$i]['imagen'];
-    ?>
-        <div id="cardtransporte" class="card col-md-3 justify-content-center">
-            <img id="imagen" src="<?= $arrComunidades[$i]['imagen'] ?>" alt="imagen de la comunidad" class="card-img-top" alt="imagen del comunidad" style="width: 300px; height:300px;">
-            <div>
-                <h4 class="nombre"><?= $arrComunidades[$i]['nombre_com'] ?></h4>
-                <h4 class="nombre">Ubicacion: <?= $arrComunidades[$i]['provincia'] ?>, <?= $arrComunidades[$i]['canton'] ?>, <?= $arrComunidades[$i]['distrito'] ?></h4>
-                <p id="descripcion" class="card-text"><?= $arrComunidades[$i]['descripcion'] ?></p>
-                <!-- <center><a href="#" class="btn btn-info">Ver más informacion</a></center> -->
+    <style>
+        .card {
+            width: 100;
+            margin: 30px;
+            /* align-items: center; */
+            align-self: center;
+            text-align: justify;
+        }
+
+        .info {
+            position: relative;
+            width: 100%;
+            height: 500px;
+            background-color: #fff;
+        }
+    </style>
+
+    <center>
+        <br>
+        <h1>Comunidades</h1>
+        <br>
+    </center>
+
+    <div>
+        <?php
+        // dep($arrTransporte);
+        for ($i = 0; $i < count($arrComunidades); $i++) {
+        ?>
+
+            <div class='card'>
+                <div class='info'>
+                    <div>
+                        <img src="<?= $arrComunidades[$i]['imagen'] ?>" alt="imagen de la comunidad" class="card-img-top" alt="imagen del comunidad" style="width: 300px; height:300px;">
+                    </div>
+                    <div>
+                        <h1 class='comunidad'><?= $arrComunidades[$i]['nombre_com'] ?></h1>
+                        <h2 class="ubicacion"><?= $arrComunidades[$i]['provincia'] ?>, <?= $arrComunidades[$i]['canton'] ?>, <?= $arrComunidades[$i]['distrito'] ?>.</h2>
+                        <p class='descripcion'><?= $arrComunidades[$i]['descripcion'] ?></p>
+                    </div>
+                </div>
             </div>
-        </div>
-    <?php
-    }
-    ?>
-</div>
+
+        <?php
+        }
+        ?>
+
+    </div>
+
+    <!-- <div class='card'>
+            <div class='info'>
+                <img src="..." alt="imagen de la comunidad" class="card-img-top" alt="imagen del comunidad" style="width: 300px; height:300px;">
+                <h1 class='comunidad'>Nombre</h1>
+                <h2 class="ubicacion">Puntarenas, Lepanto, Lepanto.</h2>
+                <p class='descripcion'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius esse corporis, velit porro impedit laudantium accusamus! Id velit, illum magni rem mollitia blanditiis iste maiores optio ipsa, est dolorem fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius esse corporis, velit porro impedit laudantium accusamus! Id velit, illum magni rem mollitia blanditiis iste maiores optio ipsa, est dolorem fugit.</p>
+            </div>
+     </div> -->
+
+    <div class="flex-container">
+
+    </div>
+
+</main>
+
 <?php
 // dep($arrComunidades);
 footer($data);
