92af1c240c3c84e8f87a8c879e6294976a0a33e0
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2022-11-22 19:03:35 -0600

    Codigo de cards

--- Views/Servicios/hospedaje.php
+++ Views/Servicios/hospedaje.php
@@ -2,10 +2,13 @@
 _header($data);
 $arrHospedaje =  $data['hospedaje'];
 ?>
+   <link href="<?= media(); ?>/css/cards-Hospedaje/cards_hospedaje.css" rel="stylesheet">
 
-<main>
-
-    <center>
+   <br>
+ <br>
+ <br>
+ <br>
+ <center>
         <br>
         <h1 class="titulo" style="color: #0f265c"><b><?= $data['page_title'] ?></b></h1>
         <br>
@@ -14,77 +17,32 @@ $arrHospedaje =  $data['hospedaje'];
 
     </center>
 
-    <div class="container-grupos">
-        <?php
-         for ($i = 0; $i < count($arrHospedaje); $i++) {
-            $arrHospedaje[$i]['imagen']; 
-        ?>
-            <div class="card-grupos">
-            <center>
-                <img id="imagen" src="<?= $arrHospedaje[$i]['imagen'] ?>" alt="Imagen del hospedaje" class="card-img-top" alt="Imagen del hospedaje" style="width: 300px; height:300px;">
-                </center>
-
-                <div>
-                    <span>
-                    <h4 class="nombre"><?= $arrHospedaje[$i]['nombre_hosp'] ?></h4>
-                    </span>
-                    <span id="hideText" class="hideText">
-                        <p>
-                    
-                        </p>
-                        <p>
-                        <b>Información del hospedaje</b><br>
-                        <br>
-                        <b>Tipo de hospedaje:</b> <?= $arrHospedaje[$i]['tipo'] ?>
-                        <br>
-                        <b>Precio:</b> <?= $arrHospedaje[$i]['precio'] ?>
-                        
-                        </p>
-                        <p>
-                            <b>Información de contacto</b><br>
-                            <b>Teléfono:</b> <?= $arrHospedaje[$i]['telefono'] ?>
-                            <br>
-                            <b>Dirección:</b> <?= $arrHospedaje[$i]['direccion'] ?>
-                        </p>
-                    </span>
-
-                    <center>
-                        <button class="readMore_btn" id="readMore_btn" onclick="toggleText()">Ver más</button>
-                    </center>
-
-                </div>
-
-            </div>
-
-        <?php
-        }
-        ?>
-    </div>
-</main>
+<div class="row">
+    <?php
+    // dep($arrGrupos);
+    for ($i = 0; $i < count($arrHospedaje); $i++) {
+        $arrHospedaje[$i]['imagen'];
+    ?>
+  <section class="programs">
+		<a href="#">
+			<div class="content">          
+                <p id="" class="textNombre"><?= $arrHospedaje[$i]['nombre_hosp'] ?></p>
+                <p id="" class="textTipo">Tipo: <?= $arrHospedaje[$i]['tipo'] ?></p>
+                <p id="" class="textPrecio">Precio: <?= $arrHospedaje[$i]['precio'] ?></p>
+                <p id="" class="textTel">Telefono: <?= $arrHospedaje[$i]['telefono'] ?></p>
+                <p id="" class="textDic">Dirección: <?= $arrHospedaje[$i]['direccion'] ?></p>
+            <ul>
+			
+				</ul>	
+			</div>
+		</a>
+        <img id="imagen" src="<?= $arrHospedaje[$i]['imagen'] ?>" alt="Imagen del hospedaje" class="card-img-top" alt="Imagen del hospedaje" style="width: 300px; height:300px;">
+	</section>
+    <?php
+    }
+    ?>
+</div>
 
 <?php
 footer($data);
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
